cd106284c1080ddca9381800d67ba53ab9602d66
namira121 <127211008+namira121@users.noreply.github.com>
2023-05-24 11:03:08 +0700

    Add project files.

---
+++ paradigmaoop/paradigmaoop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class orang {
+public:
+	string nama;
+
+	orang(string, pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl; 
+	}
+
+};
