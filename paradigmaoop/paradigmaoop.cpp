050c6b6f0bb6d12a619ad6c309ff145fc3ede526
namira121 <127211008+namira121@users.noreply.github.com>
2023-05-24 11:19:09 +0700

    memasukkan member ke dalam class pelajar yaitu variabel sekolah, pelajar(constructor), ~pelajar(destructor), dan string untuk perkenalan

--- paradigmaoop/paradigmaoop.cpp
+++ paradigmaoop/paradigmaoop.cpp
@@ -23,4 +23,20 @@ public:
 class pelajar : public orang {
 public:
 	string sekolah;
+
+	pelajar(string pNama, string pSekolah) :
+		orang(pNama),
+		sekolah(pSekolah) {
+		cout << "pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return "Halo, nama saya " + nama + "dari sekolah " + sekolah + "\n\n";
+	}
 };
+
+int main() {
+
+}
