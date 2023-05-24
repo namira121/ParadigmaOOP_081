62ace6a9bb391093f31c4b89ec660a0484d00349
namira121 <127211008+namira121@users.noreply.github.com>
2023-05-24 11:08:46 +0700

    memasukkan member ke dalam class yaitu variabel nama, orang(constructor), ~orang(destructor), dan function jumlah

--- paradigmaoop/paradigmaoop.cpp
+++ paradigmaoop/paradigmaoop.cpp
@@ -6,9 +6,16 @@ class orang {
 public: 
 	string nama;
 
-	orang(string, pNama) :
+	orang(string pNama) :
 		nama(pNama) {
 		cout << "orang dibuat\n" << endl; 
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+
+	int jumlah(int a, int b) {
+		return a + b;
 	}
 
 };
